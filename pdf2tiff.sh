11d2dcc743b3eb9bbc4a3bfd0318147fcbc96b27
Rasan Rasch <rasan@nyu.edu>
2017-07-31 20:14:39 -0400

    Set resolution and compress tiff files

--- pdf2tiff.sh
+++ pdf2tiff.sh
@@ -13,17 +13,24 @@ echo $NUM_PAGES
 pdfseparate "$PDF_FILE" page-%d.pdf
 
 page_num=1
+
+get_file_name ()
+{
+	printf 'out-%06d.tif' $page_num
+}
+
 for i in `seq 1 $NUM_PAGES`; do
 	echo $i
-	gs -q -dNOPAUSE -sDEVICE=tiff24nc -dBATCH \
+	gs -q -dNOPAUSE -sDEVICE=tiff24nc -dBATCH -r300 \
 		-sOutputFile=page-$i.tif page-$i.pdf
 	if [ $i -gt 1 -a $i -lt $NUM_PAGES ]; then
-		convert page-$i.tif -crop 50%x100% +repage half-%d.tif
-		mv half-0.tif out-$page_num.tif
+		convert page-$i.tif -crop 50%x100% +repage \
+			-compress lzw half-%d.tif
+		mv half-0.tif `get_file_name $page_num`
 		page_num=$((page_num+1))
-		mv half-1.tif out-$page_num.tif
+		mv half-1.tif `get_file_name $page_num`
 	else
-		mv page-$i.tif out-$page_num.tif
+		convert page-$i.tif -compress lzw `get_file_name $page_num`
 	fi
 	page_num=$((page_num+1))
 done
