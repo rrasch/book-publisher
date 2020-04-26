60b4ff4451709fe3312f8b23b3ced093a3e9a607
Rasan Rasch <rasan@nyu.edu>
2020-04-26 07:19:06 -0400

    Set option for grayscale

--- pdf-shrink.sh
+++ pdf-shrink.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-gs -sDEVICE=pdfwrite \
-	-sColorConversionStrategy=Gray \
-	-dProcessColorModel=/DeviceGray \
+# Shrink size of PDF by resampling images to 72pi.
+
+while getopts "g" opt; do
+	case $opt in
+		g)
+			OPTS="-sColorConversionStrategy=Gray"
+			OPTS="$OPTS -dProcessColorModel=/DeviceGray"
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+gs -sDEVICE=pdfwrite $OPTS \
 	-dCompatibilityLevel=1.4 \
 	-dPDFSETTINGS=/screen \
 	-dNOPAUSE -dQUIET -dBATCH \
