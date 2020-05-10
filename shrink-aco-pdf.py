0aaf539a994b784278c63aff530ec0dda5419b28
Rasan Rasch <rasan@nyu.edu>
2020-05-10 15:27:38 -0400

    Code cleanup

--- shrink-aco-pdf.py
+++ shrink-aco-pdf.py
@@ -2,26 +2,39 @@
 
 import glob
 import os
+import shutil
 import subprocess
 import sys
 import tempfile
-import time
+
+
+def do_cmd(cmd, **kwargs):
+    print("Running command:", " ".join(cmd))
+    try:
+        process = subprocess.run(cmd, check=True, **kwargs)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+if len(sys.argv) != 3:
+    print("Usage: {} INPUT_FILE OUTPUT_FILE".format(sys.argv[0]))
+    exit(1)
 
 input_file = sys.argv[1]
-print(input_file)
+output_file = sys.argv[2]
+print("Input file:", input_file)
+print("Output file:", output_file)
+
+if input_file == output_file:
+    print("Input file can't be the same as output file.")
+    exit(1)
 
 # tmpdir = tempfile.TemporaryDirectory()
+# print(tmpdir.name)
 tmpdir = tempfile.mkdtemp()
-#print(tmpdir.name)
 print(tmpdir)
 
-# time.sleep(30)
-
-
-process = subprocess.run(['qpdf',
-    '--split-pages', input_file, '{}/%d.pdf'.format(tmpdir)])
-
-print(process)
+do_cmd(['qpdf', '--split-pages', input_file, '{}/%d.pdf'.format(tmpdir)])
 
 for pdf_file in sorted(glob.glob(f"{tmpdir}/*.pdf")):
     basename = os.path.splitext(pdf_file)[0]
@@ -29,15 +42,14 @@ for pdf_file in sorted(glob.glob(f"{tmpdir}/*.pdf")):
     hocr_file = basename + ".hocr"
     old_jpg_file = basename + "-000.jpg"
     new_jpg_file = basename + ".jpg"
-    process = subprocess.run(['pdfimages', '-all', pdf_file, basename])
-    process = subprocess.run(['convert', old_jpg_file,
-        '-quality', '10', new_jpg_file])
+    do_cmd(['pdfimages', '-all', pdf_file, basename])
+    do_cmd(['convert', old_jpg_file, '-quality', '10', new_jpg_file])
     os.remove(old_jpg_file)
-    process = subprocess.run(['pdf2djvu', '-o', djvu_file, pdf_file])
+    do_cmd(['pdf2djvu', '-o', djvu_file, pdf_file])
     with open(hocr_file, 'w') as f:
-        process = subprocess.run(['djvu2hocr', djvu_file],
-            stdout=f)
+        do_cmd(['djvu2hocr', djvu_file], stdout=f)
 
-#tmpdir.cleanup()
+do_cmd(['hocr-pdf', '--savefile', output_file, tmpdir])
 
+shutil.rmtree(tmpdir)
 
