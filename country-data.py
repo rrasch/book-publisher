571b8831f397cb80315614285aae0a6c68bb8ba8
Rasan Rasch <rasan@nyu.edu>
2020-07-12 09:01:53 -0400

    Add script to print data for country

---
+++ country-data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+import countryinfo
+
+# number of sq kilometers per square miles
+conv_ratio = 1.609344 ** 2
+
+def convert_sqmiles(sq_km):
+    return float(sq_km) / conv_ratio
+
+parser = argparse.ArgumentParser(description="Get country info")
+parser.add_argument("country")
+args = parser.parse_args()
+
+cinfo = countryinfo.CountryInfo(args.country)
+try:
+    print(convert_sqmiles(cinfo.area()))
+except KeyError:
+    exit(1)
+
