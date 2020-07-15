15da2f31be3a7f49349d8fec9e9735f007c9578d
Rasan Rasch <rasan@nyu.edu>
2020-07-14 20:28:18 -0400

    Add support for looking up city data

--- country-data.py
+++ country-data.py
@@ -2,12 +2,18 @@
 
 import argparse
 import countryinfo
+import json
+import os
+import sys
 
-# number of sq kilometers per square miles
-conv_ratio = 1.609344 ** 2
+# geojson data file for world cities
+city_file = 'ne_50m_populated_places.geojson'
 
-def convert_sqmiles(sq_km):
-    return float(sq_km) / conv_ratio
+# number of sq kilometers per square mile
+RATIO_KM2_TO_MILE2 = 1.609344 ** 2
+
+def convert_km2_to_mile2(km2):
+    return float(km2) / RATIO_KM2_TO_MILE2
 
 parser = argparse.ArgumentParser(description="Get country info")
 parser.add_argument("country")
@@ -15,7 +21,22 @@ args = parser.parse_args()
 
 cinfo = countryinfo.CountryInfo(args.country)
 try:
-    print(round(convert_sqmiles(cinfo.area())))
+    print(convert_km2_to_mile2(cinfo.area()))
+    exit(0)
+except KeyError:
+    pass
+
+app_home = os.path.dirname(os.path.abspath(sys.argv[0]))
+city_file = os.path.join(app_home, city_file)
+
+area = {}
+with open(city_file) as f:
+    city_data = json.load(f)
+for feature in city_data['features']:
+    prop = feature['properties']
+    area[prop['NAME']] = prop['MAX_AREAKM']
+try:
+    print(area[args.country])
 except KeyError:
     exit(1)
 
