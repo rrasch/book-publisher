4df2652ad8c4f3cdbdb0efc356e3d48dcf966f7e
Rasan Rasch <rasan@nyu.edu>
2020-07-16 01:56:53 -0400

    Use xpath to find books without geo subject

--- find-missing-geo.sh
+++ find-missing-geo.sh
@@ -10,10 +10,14 @@ do
     partner=${tmp##*/}
     for i in `ls $rstar_dir/wip/se`; do
         digid=${i##*/}
-        coord_file="${rstar_dir}/wip/se/${digid}/aux/${digid}_geo_coord.json"
-        if [ ! -f "$coord_file" ]; then
-            echo $digid
-        fi
+#       coord_file="${rstar_dir}/wip/se/${digid}/aux/${digid}_geo_coord.json"
+#       if [ ! -f "$coord_file" ]; then
+#           echo $digid
+#       fi
+        mods_file=`ls ${rstar_dir}/wip/se/${digid}/data/*mods.xml`
+		match=`xpath -q -e \
+			"//subject[@authority='lcsh']/geographic" $mods_file`
+		[ -z "$match" ] && echo $digid
     done
 done
 
